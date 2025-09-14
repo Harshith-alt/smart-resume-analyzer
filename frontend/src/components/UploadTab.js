2509ef144df73e9bf1997e707a1c6377beb10727
Harshith-alt <mh21ceb0a26@student.nitw.ac.in>
2025-09-15 02:06:17 +0530

    Initial commit

--- frontend/src/components/UploadTab.js
+++ frontend/src/components/UploadTab.js
@@ -29,7 +29,7 @@ function UploadTab() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/upload", // ✅ backend route
+        "http://127.0.0.1:8000/api/upload",
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
